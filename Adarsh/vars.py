e5ce8e0c5c48a9b8da9f3607f4d776a4ca2094a3
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2023-05-06 08:45:45 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     API_ID = int(getenv('API_ID', '14505719'))
     API_HASH = str(getenv('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', 'BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'rxmovies'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'rxmoviesdaa'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001660274107'))
@@ -24,10 +24,10 @@ class Var(object):
     NO_PORT = bool(getenv('NO_PORT', False))
     SHORTENER_API = str(getenv('SHORTENER_API', 'b6aace46d40c605fff8e0cafbcd8fbe416851f4d'))
     SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnlink.in'))
-    FDQN = str(getenv('FDQN', 'https://rxmovies.onrender.com'))
+    FDQN = str(getenv('FDQN', 'https://rxmoviesdaa.onrender.com'))
 
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('JON SNOW'))
+    OWNER_USERNAME = str(getenv('JonSnow11'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME', 'Rxmovies'))
